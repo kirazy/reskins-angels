7f2aaa71ad0ad9400211b472d31adbd1cde5531f
Kirazy <35714562+kirazy@users.noreply.github.com>
2020-08-07 04:43:18 -0700

    Preliminary settings and locale buildout

--- settings-updates.lua
+++ settings-updates.lua
@@ -3,7 +3,6 @@
 --     
 -- See LICENSE.md in the project directory for license information.
 
--- Pure Angels uses 4 tiers (for now?), so hide extra color settings
 local function hide_setting(setting_type, setting_name, setting_default)
     if data.raw[setting_type] and data.raw[setting_type][setting_name] then
         data.raw[setting_type][setting_name].hidden = true
@@ -13,12 +12,17 @@ local function hide_setting(setting_type, setting_name, setting_default)
     end
 end
 
-if not mods["boblibrary"] then
-    hide_setting("string-setting", "reskins-lib-custom-colors-tier-0")
-    hide_setting("string-setting", "reskins-lib-custom-colors-tier-5")
-end
-
--- 595959
--- 2957cc
--- cc2929
--- ccae29
+-- Hide settings that are not in use
+hide_setting("bool-setting", "reskins-angels-do-angelsbioprocessing")
+hide_setting("bool-setting", "reskins-angels-do-angelsexploration")
+hide_setting("bool-setting", "reskins-angels-do-angelsindustries")
+hide_setting("bool-setting", "reskins-angels-do-angelspetrochem")
+hide_setting("bool-setting", "reskins-angels-do-angelsrefining")
+-- hide_setting("bool-setting", "reskins-angels-do-angelssmelting")
+hide_setting("bool-setting", "reskins-angels-do-angelsaddons-cab")
+hide_setting("bool-setting", "reskins-angels-do-angelsaddons-crawlertrain")
+hide_setting("bool-setting", "reskins-angels-do-angelsaddons-oresilos")
+hide_setting("bool-setting", "reskins-angels-do-angelsaddons-petrotrain")
+hide_setting("bool-setting", "reskins-angels-do-angelsaddons-pressuretanks")
+hide_setting("bool-setting", "reskins-angels-do-angelsaddons-smeltingtrain")
+hide_setting("bool-setting", "reskins-angels-do-angelsaddons-warehouses")
--- settings.lua
+++ settings.lua
@@ -4,8 +4,7 @@
 -- See LICENSE.md in the project directory for license information.
 
 -- Core settings
-data:extend(
-{
+data:extend({
     {
         type = "bool-setting",
         name = "reskins-angels-use-angels-tier-colors",
@@ -13,6 +12,148 @@ data:extend(
         order = "a",
         default_value = true,
     }
-}
+})
 
--- Settings to disable select Reskins by mod
+-- Settings available based on mod loadout
+if mods["angelsbioprocessing"] then
+    data:extend({
+        {
+            type = "bool-setting",
+            name = "reskins-angels-do-angelsbioprocessing",
+            setting_type = "startup",
+            default_value = true,
+        }
+    })
+end
+
+if mods["angelsexploration"] then
+    data:extend({
+        {
+            type = "bool-setting",
+            name = "reskins-angels-do-angelsexploration",
+            setting_type = "startup",
+            default_value = true,
+        }
+    })
+end
+
+if mods["angelsindustries"] then
+    data:extend({
+        {
+            type = "bool-setting",
+            name = "reskins-angels-do-angelsindustries",
+            setting_type = "startup",
+            default_value = true,
+        }
+    })
+end
+
+if mods["angelspetrochem"] then
+    data:extend({
+        {
+            type = "bool-setting",
+            name = "reskins-angels-do-angelspetrochem",
+            setting_type = "startup",
+            default_value = true,
+        }
+    })
+end
+
+if mods["angelsrefining"] then
+    data:extend({
+        {
+            type = "bool-setting",
+            name = "reskins-angels-do-angelsrefining",
+            setting_type = "startup",
+            default_value = true,
+        }
+    })
+end
+
+if mods["angelssmelting"] then
+    data:extend({
+        {
+            type = "bool-setting",
+            name = "reskins-angels-do-angelssmelting",
+            setting_type = "startup",
+            default_value = true,
+        }
+    })
+end
+
+if mods["angelsaddons-cab"] then
+    data:extend({
+        {
+            type = "bool-setting",
+            name = "reskins-angels-do-angelsaddons-cab",
+            setting_type = "startup",
+            default_value = true,
+        }
+    })
+end
+
+if mods["angelsaddons-crawlertrain"] then
+    data:extend({
+        {
+            type = "bool-setting",
+            name = "reskins-angels-do-angelsaddons-crawlertrain",
+            setting_type = "startup",
+            default_value = true,
+        }
+    })
+end
+
+if mods["angelsaddons-oresilos"] then
+    data:extend({
+        {
+            type = "bool-setting",
+            name = "reskins-angels-do-angelsaddons-oresilos",
+            setting_type = "startup",
+            default_value = true,
+        }
+    })
+end
+
+if mods["angelsaddons-petrotrain"] then
+    data:extend({
+        {
+            type = "bool-setting",
+            name = "reskins-angels-do-angelsaddons-petrotrain",
+            setting_type = "startup",
+            default_value = true,
+        }
+    })
+end
+
+if mods["angelsaddons-pressuretanks"] then
+    data:extend({
+        {
+            type = "bool-setting",
+            name = "reskins-angels-do-angelsaddons-pressuretanks",
+            setting_type = "startup",
+            default_value = true,
+        }
+    })
+end
+
+if mods["angelsaddons-smeltingtrain"] then
+    data:extend({
+        {
+            type = "bool-setting",
+            name = "reskins-angels-do-angelsaddons-smeltingtrain",
+            setting_type = "startup",
+            default_value = true,
+        }
+    })
+end
+
+if mods["angelsaddons-warehouses"] then
+    data:extend({
+        {
+            type = "bool-setting",
+            name = "reskins-angels-do-angelsaddons-warehouses",
+            setting_type = "startup",
+            default_value = true,
+        }
+    })
+end
