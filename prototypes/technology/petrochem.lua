ad9ec71b37a834c3fcf13f5bcc6bdf99d5a1c60a
Kirazy <35714562+kirazy@users.noreply.github.com>
2020-12-20 03:40:07 -0800

    add flare stack

--- prototypes/technology/petrochem.lua
+++ prototypes/technology/petrochem.lua
@@ -26,6 +26,8 @@ local technologies = {
     -- ["angels-advanced-chemistry-3"] = {tier = 3, prog_tier = 4, icon_name = "advanced-chemistry"},
     -- ["angels-advanced-chemistry-4"] = {tier = 4, prog_tier = 5, icon_name = "advanced-chemistry"},
 
+    -- Flare stack
+    ["angels-flare-stack"] = {icon_name = "flare-stack", technology_icon_layers = 1, technology_icon_mipmaps = 4},
 
     -- Advanced Gas Processing
     ["angels-advanced-gas-processing"] = {tier = 1, prog_tier = 3, icon_name = "advanced-gas-processing"},
