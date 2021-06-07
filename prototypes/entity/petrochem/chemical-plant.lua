fdd71bdc4218e47c556d083c780aa3a3c548453d
Akaash Suresh <casa.akaash@gmail.com>
2021-06-07 15:55:14 -0500

    Fix tiering for Oil/Gas separator & Chemical plant (#3)
    * Fix tiering for Oil/Gas separator & Chemical plant
    
    * Additional tweaks
    
    Co-authored-by: Kira <35714562+kirazy@users.noreply.github.com>

--- prototypes/entity/petrochem/chemical-plant.lua
+++ prototypes/entity/petrochem/chemical-plant.lua
@@ -21,10 +21,10 @@ local inputs = {
 }
 
 local tier_map = {
-    ["angels-chemical-plant"] = {tier = 1},
-    ["angels-chemical-plant-2"] = {tier = 2},
-    ["angels-chemical-plant-3"] = {tier = 3},
-    ["angels-chemical-plant-4"] = {tier = 4},
+    ["angels-chemical-plant"] = {tier = 1, prog_tier = 2},
+    ["angels-chemical-plant-2"] = {tier = 2, prog_tier = 3},
+    ["angels-chemical-plant-3"] = {tier = 3, prog_tier = 4},
+    ["angels-chemical-plant-4"] = {tier = 4, prog_tier = 5},
 }
 
 -- Reskin entities, create and assign extra details
