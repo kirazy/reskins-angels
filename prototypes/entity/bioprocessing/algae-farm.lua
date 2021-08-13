3917c0f048e4f5c7ba10e089541b44fe70fef629
Kira <35714562+kirazy@users.noreply.github.com>
2021-08-12 17:55:13 -0700

    Update EA compatibility

--- prototypes/entity/bioprocessing/algae-farm.lua
+++ prototypes/entity/bioprocessing/algae-farm.lua
@@ -37,7 +37,7 @@ end
 -- Extended Angel's adds a 5th tier of Algae farm in version 0.5.0, prior to that Algae farm 4 had tier 5 ingredients
 if reskins.lib.migration.is_version_or_newer(mods["extendedangels"], "0.5.0") then
     tier_map["algae-farm-5"] = {tier = 5}
-elseif reskins.lib.migration.is_older_version(mods["angelsbioprocessing"], "0.7.20") then
+elseif mods["extendedangels"] and reskins.lib.migration.is_older_version(mods["angelsbioprocessing"], "0.7.20") then
     tier_map["algae-farm-4"].prog_tier = 5
 end
 
