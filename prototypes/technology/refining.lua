50fd7142321223eb547eaf2b46de12b6cffb3dd0
Kira <35714562+kirazy@users.noreply.github.com>
2021-09-16 02:31:07 -0700

    Update ore sorting facility with high res sprites

--- prototypes/technology/refining.lua
+++ prototypes/technology/refining.lua
@@ -5,3 +5,21 @@
 
 -- Check to see if reskinning needs to be done.
 if not (reskins.angels and reskins.angels.triggers.refining.technologies) then return end
+
+-- Setup standard inputs
+local inputs = {
+    mod = "angels",
+    group = "refining",
+    type = "technology",
+    technology_icon_size = 256,
+    technology_icon_mipmaps = 4,
+}
+
+local technologies = {
+    -- Advanced Ore Refinining (Ore Sorting Facility)
+    ["advanced-ore-refining-1"] = {tier = 2, icon_name = "advanced-ore-refining"},
+    ["advanced-ore-refining-2"] = {tier = 3, icon_name = "advanced-ore-refining"},
+    ["advanced-ore-refining-3"] = {tier = 4, icon_name = "advanced-ore-refining"},
+}
+
+reskins.lib.create_icons_from_list(technologies, inputs)
