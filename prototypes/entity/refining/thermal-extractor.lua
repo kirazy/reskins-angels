3698ca26a688951ee8e46c9d61cdfc1e404bb8fd
Kira <35714562+kirazy@users.noreply.github.com>
2024-05-01 17:52:55 -0700

    feat: add icons for thermal extractor

--- prototypes/entity/refining/thermal-extractor.lua
+++ prototypes/entity/refining/thermal-extractor.lua
@@ -5,3 +5,30 @@
 
 -- Check to see if reskinning needs to be done.
 if not (reskins.angels and reskins.angels.triggers.refining.entities) then return end
+
+---@type ConstructIconInputsOld
+local inputs = {
+    type = "mining-drill",
+    icon_name = "thermal-extractor",
+    mod = "angels",
+    group = "refining",
+}
+
+-- Setup defaults.
+reskins.lib.parse_inputs(inputs)
+
+local tier_map = {
+    ["thermal-bore"] = { tier = 1, prog_tier = 2 },
+    ["thermal-extractor"] = { tier = 2, prog_tier = 3 },
+}
+
+for name, map in pairs(tier_map) do
+    local tier = map.tier
+    if reskins.lib.settings.get_value("reskins-lib-tier-mapping") == "progression-map" then
+        tier = map.prog_tier or map.tier
+    end
+
+    inputs.tint = map.tint or reskins.lib.tiers.get_tint(tier)
+
+    reskins.lib.construct_icon(name, tier, inputs)
+end
